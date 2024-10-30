c627f003871518ecf0bd0ef9ce84d4eb9e12c04a
Leekyoungwook <lkw001110code@gmail.com>
2024-10-30 15:47:29 +0900

    first commit

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { communityLinks, platformLinks, resourcesLinks } from "../../constants/data";
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+ <footer className="mt-20 border-t py-10 border-neutral-700">
+ <div className="container grid grid-cols-2 grid-cols-3">
+  <div>
+  <h3 className="footer-title">Resources</h3>
+  <ul clasName="space y-2">
+    {resourcesLinks.map((link, index) => (
+    <li key={index}>
+      <Link to={Link.to}>{link.text}</Link>
+      </li>
+      ))}
+      </ul>
+  </div>
+  <div>
+  <h3 className="footer-title">Platform</h3>
+  <ul clasName="space y-2">
+  {platformLinks.map((link,index) => (
+      <li key={index}>
+        <Link to={link.to}>{link.text} </Link>
+      </li>
+    ))}
+    </ul>
+  </div>
+  <div>
+  <ul clasName="space y-2">
+  <h3 className="footer-title">Community</h3>
+  {communityLinks.map((link, index) => (
+      <li key={index}>  
+        <Link to={link.to}>{link.text} </Link>
+      </li>
+    ))}
+    </ul>
+  </div>
+  </div>
+  </footer>
+  );
+};
+
+export default Footer;
